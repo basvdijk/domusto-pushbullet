8561d78b7daeed8800d5f058f59b8f7b9915d259
Bas van Dijk <bas@usto.nl>
2018-01-03 16:29:14 +0100

    Initial commit

--- index.ts
+++ index.ts
@@ -5,8 +5,7 @@ import config from '../../config';
 import DomustoPlugin from '../../domusto/DomustoPlugin';
 
 // INTERFACES
-import { PluginCategories } from '../../domusto/interfaces/plugin/PluginMetaData';
-import { PluginConfiguration } from '../../domusto/interfaces/plugin/PluginConfiguration';
+import { Domusto } from '../../domusto/DomustoInterfaces';
 
 // PLUGIN SPECIFIC
 import * as PushBullet from 'pushbullet';
@@ -28,12 +27,12 @@ class DomustoPushBullet extends DomustoPlugin {
      * @param {any} Plugin configuration as defined in the config.js file
      * @memberof DomustoPushBullet
      */
-    constructor(pluginConfiguration: PluginConfiguration) {
+    constructor(pluginConfiguration: Domusto.PluginConfiguration) {
 
         super({
             plugin: 'Pushbullet executer',
             author: 'Bas van Dijk',
-            category: PluginCategories.push,
+            category: Domusto.PluginCategories.push,
             version: '0.0.1',
             website: 'http://domusto.com'
         });
